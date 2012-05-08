bd90e8d97a9436d4c30417610a7b4638bdf56d2a
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2012-05-09 01:32:51 +0200

    Move WP authentication code into a separate class and make it configurable.

---
+++ nette/app/models/WordPressAuthenticator.php
@@ -0,0 +1,58 @@
+<?php
+
+use Nette\Security as NS;
+
+class WordPressAuthenticator extends Nette\Object
+{
+	/** WordPress document root relative to $_SERVER['DOCUMENT_ROOT'] (with trailing /) */
+	private $wpDocRoot;
+	/** WordPress base URL (with trailing /) */
+	private $wpUrl;
+	
+	public function __construct($wpDocRoot, $wpUrl)
+	{
+		$this->wpDocRoot = $wpDocRoot;
+		$this->wpUrl = $wpUrl;
+	}
+
+	public function getLoggedWordPressUserId()
+	{
+		// boot WordPress
+		define('WP_USE_THEMES', false);
+		global $wp_rewrite;
+		global $wp;
+		require_once($_SERVER['DOCUMENT_ROOT'].$this->wpDocRoot.'wp-load.php');
+		
+		// get the logged WP user
+		global $current_user;
+		get_currentuserinfo();
+
+		return $current_user->ID;
+	}
+	
+	/**
+	 * $return member identity
+	 */
+	public function getMemberIdentityForWpUser($context, $wpUserId)
+	{
+		$binding = $context->createUserBindings()->where(array('wp_user_id' => $wpUserId))->fetch();
+		if ($binding != FALSE) {
+			$memberId = $binding['member_id'];
+			$member = $context->createMembers()->get($memberId);
+			$userData = array(
+				'id' => $memberId,
+				'username' => $current_user->user_login,
+				'name' => $member['first_name']." ".$member['last_name'],
+				);
+			return new NS\Identity($memberId, NULL, $userData);
+		} else {
+			return NULL;
+		}
+	}
+	
+	public function getWpLoginPageUrl()
+	{
+		$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		return $this->wpUrl."wp-login.php?redirect_to=$url";
+	}
+}
--- nette/app/presenters/WordPressSignPresenter.php
+++ nette/app/presenters/WordPressSignPresenter.php
@@ -1,9 +1,19 @@
 <?php
 
-use Nette\Security as NS;
-
 class WordPressSignPresenter extends BasePresenter
 {
+	/** var WordPressAuthenticator */
+	private $wpAuthenticator;
+
+	/**
+     * @param WordPressAuthenticator
+     */
+    public function startup()
+    {
+		parent::startup();
+        $this->wpAuthenticator = $this->context->container->wpAuthenticator;
+	}
+	
 	public function actionIn()
 	{
 		if ($this->getUser()->isLoggedIn()) {
@@ -11,12 +21,12 @@ class WordPressSignPresenter extends BasePresenter
 			$this->redirect('Homepage:');
 		}
 	
-		$wpUserId = $this->getLoggedWordPressUserId();
+		$wpUserId = $this->wpAuthenticator->getLoggedWordPressUserId();
 		$loggedToWordPress = $wpUserId != 0;
 		if ($loggedToWordPress)
 		{
 			// find matching ChoirMaster member id and create their identity
-			$identity = $this->getMemberIdentityForWpUser($wpUserId);
+			$identity = $this->wpAuthenticator->getMemberIdentityForWpUser($this->context, $wpUserId);
 			if ($identity != NULL) {
 				$this->getUser()->login($identity);
 				$this->flashMessage("Přihlášení pomocí WordPressu proběhlo úspěšně.", 'success');
@@ -37,45 +47,8 @@ class WordPressSignPresenter extends BasePresenter
 		$this->redirect('Homepage:');
 	}
 	
-	private function getLoggedWordPressUserId()
-	{
-		// boot WordPress
-		define('WP_USE_THEMES', false);
-		global $wp_rewrite;
-		global $wp;
-		require_once($_SERVER['DOCUMENT_ROOT'].'/corale.cz/wordpress/wp-load.php');
-		
-		// get the logged WP user
-		global $current_user;
-		get_currentuserinfo();
-
-		return $current_user->ID;
-	}
-	
-	/**
-	 * $return identity
-	 */
-	private function getMemberIdentityForWpUser($wpUserId)
-	{
-		$binding = $this->context->createUserBindings()->where(array('wp_user_id' => $wpUserId))->fetch();
-		if ($binding != FALSE) {
-			$memberId = $binding['member_id'];
-			$member = $this->context->createMembers()->get($memberId);
-			$userData = array(
-				'id' => $memberId,
-				'username' => $current_user->user_login,
-				'name' => $member['first_name']." ".$member['last_name'],
-				);
-			return new NS\Identity($memberId, NULL, $userData);
-		} else {
-			return NULL;
-		}
-	}
-	
 	/** redirect to the WordPress login page */
-	private function redirectToWpLoginPage()
-	{
-		$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-		$this->redirectUrl("/corale.cz/wordpress/wp-login.php?redirect_to=$url");
+	public function redirectToWpLoginPage() {
+		$this->redirectUrl($this->wpAuthenticator->getWpLoginPageUrl());
 	}
 }
