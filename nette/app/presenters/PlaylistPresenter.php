d29b253d7b63b521857748babd9d28a651f517dc
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2012-06-07 00:40:04 +0200

    Implement a prototype of playlist manipulation via the Sortable plugin of the jQuery UI.
    Also implement synchronization of songs in the edited playlist and database. Make a diff and update only affected songs.

--- nette/app/presenters/PlaylistPresenter.php
+++ nette/app/presenters/PlaylistPresenter.php
@@ -32,6 +32,90 @@ class PlaylistPresenter extends BasePresenter
 		$this->redirect('edit', array('eventId' => $eventId));
 	}
 	
+	public function actionUpdate($eventId) {
+		$this->ensureLoggedUser();
+		
+		$postParams = $this->request->getPost();
+		if (!array_key_exists('songIds', $postParams)) {
+			// TODO
+			return;
+		}
+		$songIds = explode(',', $postParams['songIds']);
+		
+		Nette\Diagnostics\Debugger::dump($songIds);
+		
+		$newSongs = array();
+		$songOrders = array();
+		for ($i = 0; $i < count($songIds); $i++) {
+			$songId = intval($songIds[$i]);
+			if ($songId > 0) {
+				$newSongs[] = $songId;
+				$songOrders[$songId] = "$i";
+			}
+		}
+		
+		Nette\Diagnostics\Debugger::dump("newSongs:");
+		Nette\Diagnostics\Debugger::dump($newSongs);
+		Nette\Diagnostics\Debugger::dump("songOrders:");
+		Nette\Diagnostics\Debugger::dump($songOrders);
+		
+		// TODO: synchronize the existing and the new playlist
+		// add/delete/update the items only when there is a change
+		
+		$existingSongsSel = $this->context->createPlaylistItems()->where(array('event_id' => $eventId));
+		
+		$existingSongs = array();
+		$existingSongOrders = array();
+		foreach ($existingSongsSel as $song) {
+			$songId = $song->song_id;
+			$existingSongs[] = $songId;
+			$existingSongOrders[$songId] = $song->ord;
+		}
+		
+		// make a copy to be further filtered
+		$songsToDelete = $existingSongs;
+		
+		Nette\Diagnostics\Debugger::dump("existingSongs:");
+		Nette\Diagnostics\Debugger::dump($existingSongs);
+		Nette\Diagnostics\Debugger::dump("existingSongOrders:");
+		Nette\Diagnostics\Debugger::dump($existingSongOrders);
+		
+		
+		foreach ($newSongs as $song) {
+			// TODO: this smells with quardratic complexity
+			$key = array_search($song, $existingSongs);
+			$found = ($key !== FALSE);
+			
+			$newOrder = $songOrders[$song];
+			if ($found) {
+				$existingOrder = $existingSongOrders[$existingSongs[$key]];
+				if ($newOrder != $existingOrder) {
+					echo "updating song id $song order '$existingOrder' -> '$newOrder'<br>";
+					$this->context->createPlaylistItems()
+						->where(array('event_id' => $eventId, 'song_id' => $song))
+						->update(array('ord' => $newOrder));
+				}
+				unset($songsToDelete[$key]);
+			} else {
+				echo "inserting song id $song with order $newOrder<br>";
+				$this->context->createPlaylistItems()->insert(
+					array('event_id' => $eventId, 'song_id' => $song, 'ord' => $newOrder)
+				);
+			}
+		}
+		Nette\Diagnostics\Debugger::dump("songsToDelete:");
+		Nette\Diagnostics\Debugger::dump($songsToDelete);
+		foreach ($songsToDelete as $song) {
+			echo "deleting song id $song<br>";
+			$this->context->createPlaylistItems()
+				->where(array('event_id' => $eventId, 'song_id' => $song))
+				->delete();
+		}
+		
+		$this->flashMessage("Playlist byl upraven.");
+		//$this->redirect('edit', array('eventId' => $eventId));
+	}
+	
 	public function actionDelete($eventId, $songId) {
 		$this->ensureLoggedUser();
 		
@@ -47,10 +131,14 @@ class PlaylistPresenter extends BasePresenter
 		$this->eventId = $eventId;
 	}
 	
-	public function actionEdit($eventId) {
-		$this->eventId = $eventId;
-		
+	public function actionEdit($eventId, $query) {
 		$this->ensureLoggedUser();
+	
+		// TODO: show not found page on non-existent event
+	
+		$this->eventId = $eventId;
+		$this->songQuery = $query;
+		$this["songSearchForm"]->setDefaults(array('query' => $this->songQuery));
 	}
 	
 	public function renderDefault() {
@@ -61,6 +149,15 @@ class PlaylistPresenter extends BasePresenter
 	public function renderEdit() {
 		$this->template->eventId = $this->eventId;
 		$this->template->playlist = $this->getPlaylist($this->eventId);
+		
+		$q = $this->context->createSongs();
+		if ($this->songQuery) {
+			$term = "%$this->songQuery%";
+			$q->where("title LIKE ? OR author LIKE ? OR description LIKE ?",
+				array($term, $term, $term));
+		}
+		$q->order('title');
+		$this->template->repertoire = $q;
 	}
 	
 	public function getPlaylist($eventId) {
@@ -72,27 +169,7 @@ JOIN corale_event e ON e.id = p.event_id
 WHERE p.event_id = ?
 ORDER BY p.ord, s.title
 EOQ;
-			return $this->context->createPlaylistItems()->getConnection()->query($query, $eventId);
-	}
-
-	// TODO: use AJAX
-	public function actionAdd($eventId, $query)
-	{
-		$this->eventId = $eventId;
-		$this->songQuery = $query;
-		$this["songSearchForm"]->setDefaults(array('query' => $this->songQuery));
-	}
-	
-	public function renderAdd() {
-		$q = $this->context->createSongs();
-		if ($this->songQuery) {
-			$term = "%$this->songQuery%";
-			$q->where("title LIKE ? OR author LIKE ? OR description LIKE ?",
-				array($term, $term, $term));
-		}
-		$q->order('title');
-		$this->template->songs = $q;
-		$this->template->eventId = $this->eventId;
+		return $this->context->createPlaylistItems()->getConnection()->query($query, $eventId);
 	}
 	
 	protected function createComponentSongSearchForm()
@@ -101,5 +178,9 @@ EOQ;
 		$form->addText('query', 'Hledaný text');
 		$form->addSubmit('search', 'Hledat');
 		return $form;
+	}
+	
+	protected function createComponentPlaylistEditor() {
+		return new PlaylistEditor();
 	}
 }
